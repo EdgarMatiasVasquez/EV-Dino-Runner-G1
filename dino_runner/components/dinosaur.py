850d8c21e60a2899e95fb0ce81a6c0e1d841b6ac
EdgarMatiasVasquez <Edgar.Vasquez@fundacion-jala.org>
2023-05-01 20:35:00 -0400

    Class3: Obstáculos, en esta clases añadimos los Cactus que vendrían a ser obstáculos

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -2,11 +2,11 @@ import pygame
 from pygame.sprite import Sprite
 
 from dino_runner.utils.constants import DUCKING, JUMPING, RUNNING
+
 JUMP_VELOCITY = 8.5
 DINO_RUNNING = "running"
 DINO_JUMPING = "jumping"
 DINO_DUCKING = "ducking"
-
 
 
 class Dinosaur(Sprite): #PasCalCase
@@ -25,20 +25,18 @@ class Dinosaur(Sprite): #PasCalCase
     def update(self, user_input):
         if self.action == DINO_RUNNING:
             self.run()
-
         elif self.action == DINO_JUMPING:
            self.jump()
-
         elif self.action == DINO_DUCKING:
            self.duck()
 
-        if user_input[pygame.K_UP] and self.action != DINO_JUMPING:
-             self.action = DINO_JUMPING
-
-        elif user_input[pygame.K_DOWN] and self.action != DINO_JUMPING:
-            self.action = DINO_DUCKING
-        elif not (self.action == DINO_JUMPING or user_input[pygame.K_DOWN]):
-            self.action = DINO_RUNNING
+        if self.action != DINO_JUMPING:
+            if user_input[pygame.K_UP]:
+                self.action = DINO_JUMPING
+            elif user_input[pygame.K_DOWN]:
+                self.action = DINO_DUCKING
+            else:
+                self.action = DINO_RUNNING
         
         if self.step >=10:
            self.step = 0
@@ -47,13 +45,15 @@ class Dinosaur(Sprite): #PasCalCase
         screen.blit(self.image,(self.rect.x, self.rect.y))
 
     def run(self):
+        self.rect.y = self.POS_Y
         self.image = RUNNING[0] if self.step < 5 else RUNNING[1]
         self.step += 1
 
     def jump(self):
+        pos_y = self.rect.y - self.jump_velocity * 4
         self.image = JUMPING
-        self.rect.y -= self.jump_velocity * 4
         self.jump_velocity -= 0.8
+        self.rect.y = pos_y
         if self.jump_velocity < -JUMP_VELOCITY:
             self.rect.y = self.POS_Y
             self.action = DINO_RUNNING
@@ -61,8 +61,9 @@ class Dinosaur(Sprite): #PasCalCase
 
     def duck(self):
         self.rect.y = self.POS_Y_DUCK
-        self.image = DUCKING[0] if self.step < 5 else DUCKING[1]
+        self.image = DUCKING[0] if self.step < 5 else DUCKING[1] 
         self.step += 1
+    
         
             
  
